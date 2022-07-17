404bf2333f2fde3321da621042af179bce8f7ff7
Anna Niewiarowska <niewiarowska.as@gmail.com>
2022-07-17 12:17:42 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+{
+    const tasks = [
+        {
+            content: "wypełnić listę",
+            done: false,
+        },
+        {
+            content: "napisać zadanie",
+            done: false,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            let htmlString = "";
+            for (const task of tasks) {
+                htmlString += `
+                <li>
+                ${task.content}
+                </li>
+                `;
+            }
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+
+    };
+
+    init();
+}
