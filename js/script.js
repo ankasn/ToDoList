ac815160971a175fa13ce46082a48bc30444c1b5
Anna Niewiarowska <niewiarowska.as@gmail.com>
2022-07-18 14:19:57 +0200

    css styles added, js functions added images added

--- js/script.js
+++ js/script.js
@@ -1,34 +1,87 @@
 {
-    const tasks = [
-        {
-            content: "wypełnić listę",
-            done: false,
-        },
-        {
-            content: "napisać zadanie",
-            done: false,
-        },
-    ];
+    const tasks = [];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        })
+
+        render();
+    }
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done
+        render();
+    }
+
+    const removeEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            })
+        });
+    }
+
+    const toggleEvents = () => {
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            })
+        });
+    }
 
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
-            let htmlString = "";
-            for (const task of tasks) {
-                htmlString += `
-                <li>
-                ${task.content}
-                </li>
-                `;
-            }
+            htmlString += `
+        <li class="list__item">
+            <button class="list__button list__button-green js-done">${task.done ? "✓" : ""}
+            </button>
+            <span class=${task.done ? " list__item--done" : ""}>${task.content}</span>
+            <button class="list__button list__button--red js-remove">🗑</button>
+        </li>
+        `;
         }
+
         document.querySelector(".js-tasks").innerHTML = htmlString;
+        removeEvents();
+        toggleEvents();
     };
+
+    const clearInput = () => {
+        const formInput = document.querySelector(".js-newTask");
+        formInput.value = "";
+
+        formInput.focus();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+        clearInput();
+    }
 
     const init = () => {
         render();
 
+        const form = document.querySelector(".js-form")
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
